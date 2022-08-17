fb9e1708f1be0a91a3ce9c964b0c770a8a4b5589
lacunag <luis.acuna2008@gmail.com>
2022-08-17 10:27:00 -0400

    add url section

--- SCRAPY_WEPLAY/spiders/winpy_spider.py
+++ SCRAPY_WEPLAY/spiders/winpy_spider.py
@@ -1,11 +1,22 @@
 from urllib.error import URLError
+from xml.etree.ElementTree import Comment
 import scrapy
 
 class WinpySpider(scrapy.Spider):
     name = 'winpy'
     custom_settings = { 'DOWNLOD_DELAY': 5 }
     allowed_domains = ['www.weplay.cl']
-    start_urls = ['https://www.weplay.cl/consolas/consolas-ps5.html', 'https://www.weplay.cl/consolas/consolas-switch.html']
+
+    # URL SECTION
+    start_urls = [
+        'https://www.weplay.cl/consolas/consolas-ps5.html', 
+        'https://www.weplay.cl/consolas/consolas-switch.html',
+        'https://www.weplay.cl/consolas/consolas-xbox-sx.html',
+        'https://www.weplay.cl/consolas/consolas-xbox-one.html',
+        'https://www.weplay.cl/consolas/consolas-xbox-360.html',
+        'https://www.weplay.cl/consolas/consolas-ps3.html',
+        'https://www.weplay.cl/consolas/consolas-ps4.html',
+    ]
 
     def parse(self, response):
         for title in response.css('.product-item-details'):
