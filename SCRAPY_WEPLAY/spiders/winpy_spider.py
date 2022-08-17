46aeff8eec202ae94ad11dfc89263a51da4d2614
lacunag <luis.acuna2008@gmail.com>
2022-08-17 10:21:22 -0400

    define delay

--- SCRAPY_WEPLAY/spiders/winpy_spider.py
+++ SCRAPY_WEPLAY/spiders/winpy_spider.py
@@ -3,6 +3,7 @@ import scrapy
 
 class WinpySpider(scrapy.Spider):
     name = 'winpy'
+    custom_settings = { 'DOWNLOD_DELAY': 5 }
     allowed_domains = ['www.weplay.cl']
     start_urls = ['https://www.weplay.cl/consolas/consolas-ps5.html', 'https://www.weplay.cl/consolas/consolas-switch.html']
 
