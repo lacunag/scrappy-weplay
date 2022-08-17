f5ab7d25b2f8bfb61b7ed3d713eefd14540679f8
lacunag <luis.acuna2008@gmail.com>
2022-08-17 10:20:17 -0400

    scraping

---
+++ SCRAPY_WEPLAY/spiders/winpy_spider.py
@@ -0,0 +1,14 @@
+from urllib.error import URLError
+import scrapy
+
+class WinpySpider(scrapy.Spider):
+    name = 'winpy'
+    start_urls = ['https://www.weplay.cl/consolas/consolas-ps5.html', 'https://www.weplay.cl/consolas/consolas-switch.html']
+
+    def parse(self, response):
+        for title in response.css('.product-item-details'):
+            yield {'title': title.css('.product-item-link::text').get().strip(), 'link': title.css('::attr(href)').get(), 'price': title.css('.price::text').get(), 'special_price': title.css('.special-price .price ::text').get()}
+
+        for next_page in response.css('a.next'):
+            yield response.follow(next_page, self.parse)
+
